56a4b71e974399a9b5afa3857c3068695751d52b
Gautam Kumrawat <112876357+usegautam02@users.noreply.github.com>
2023-10-15 07:14:28 +0530

    container config

---
+++ main.js
@@ -0,0 +1,8 @@
+// khud kee projects baanaooooo
+// project based learning 
+// away from tutorial hell
+// watch tutorial mana ni hai but khud ka bhi project banaooo...time lagao repetition se chije ayegi   ----------   Go back to basic kg- 12 th  thing
+
+
+console.log("Gautam")
+
